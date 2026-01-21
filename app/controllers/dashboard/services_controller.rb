d1fd5e05700b089542005330d4f5d1bae5af8766
Hansa Gennoo <hansagennoo10@gmail.com>
2026-01-22 01:53:38 +0400

    Changed services controller

--- app/controllers/dashboard/services_controller.rb
+++ app/controllers/dashboard/services_controller.rb
@@ -1,11 +1,49 @@
 module Dashboard
-  class ServicesController < Dashboard::BaseController
+  class ServicesController < BaseController
+    before_action :set_service, only: %i[show edit update destroy]
+
     def index
-      @services = current_user.services
+      @services = current_user.services.order(created_at: :desc)
     end
+
+    def show; end
 
     def new
       @service = current_user.services.new
+    end
+
+    def create
+      @service = current_user.services.new(service_params)
+      if @service.save
+        redirect_to dashboard_service_path(@service), notice: "Service created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @service.update(service_params)
+        redirect_to dashboard_service_path(@service), notice: "Service updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @service.destroy
+      redirect_to dashboard_services_path, notice: "Service deleted."
+    end
+
+    private
+
+    def set_service
+      @service = current_user.services.find(params[:id])
+    end
+
+    def service_params
+      params.require(:service).permit(:title, :description, :duration_minutes, :price_cents, :active)
     end
   end
 end
